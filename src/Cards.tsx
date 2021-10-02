2a635bbbd8a7e9a280b0c0a727b7431124ca206b
Patrick <patrick.pospischil@outlook.de>
2021-10-02 18:01:05 +0200

    added react-smooth-dnd example

---
+++ src/Cards.tsx
@@ -0,0 +1,194 @@
+import React, { Component } from "react";
+import { Container, Draggable } from "react-smooth-dnd";
+import { applyDrag, generateItems } from "./Utils";
+
+const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+const columnNames = ["Lorem", "Ipsum", "Consectetur", "Eiusmod"];
+
+const cardColors = [
+  "azure",
+  "beige",
+  "bisque",
+  "blanchedalmond",
+  "burlywood",
+  "cornsilk",
+  "gainsboro",
+  "ghostwhite",
+  "ivory",
+  "khaki"
+];
+const pickColor = () => {
+  let rand = Math.floor(Math.random() * 10);
+  return cardColors[rand];
+};
+
+export interface Props {
+    children?: React.ReactNode
+}
+
+export interface TasksContainer{
+    id: string,
+    type: string,
+    name: string,
+    props: {
+        orientation: string,
+        className: string
+    },
+    children: Tasks
+}
+
+export interface Tasks{
+    type: string,
+    id: string,
+    props:{
+        className: string,
+        style: string
+    },
+    data: string
+}
+
+export interface State {
+    scene: {
+        type: string,
+        props: {
+            orientation: string
+        },
+        children:any
+    }
+}
+
+class Cards extends React.Component<Props, State> {
+  constructor(pProps:Props) {
+
+    super(pProps);
+
+    this.onColumnDrop = this.onColumnDrop.bind(this);
+    this.onCardDrop = this.onCardDrop.bind(this);
+    this.getCardPayload = this.getCardPayload.bind(this);
+    this.state = {
+      scene: {
+        type: "container",
+        props: {
+          orientation: "horizontal"
+        },
+        children: generateItems(4, (i:any) => ({
+          id: `column${i}`,
+          type: "container",
+          name: columnNames[i],
+          props: {
+            orientation: "vertical",
+            className: "card-container"
+          },
+          children: generateItems(+(Math.random() * 10).toFixed() + 5, (j:any) => ({
+            type: "draggable",
+            id: `${i}${j}`,
+            props: {
+              className: "card",
+              style: { backgroundColor: pickColor() }
+            },
+            data: lorem.slice(0, Math.floor(Math.random() * 150) + 30)
+          }))
+        }))
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className="card-scene">
+        <Container
+          orientation="horizontal"
+          onDrop={this.onColumnDrop}
+          dropPlaceholder={{
+            animationDuration: 150,
+            showOnTop: true,
+            className: 'cards-drop-preview'
+          }}
+        >
+          {this.state.scene.children.map((column:any):any => {
+            return (
+              <Draggable key={column.id}>
+                <div className={column.props.className}>
+                  <div className="card-column-header">
+                    
+                    {column.name}
+                  </div>
+                  <Container
+                    {...column.props}
+                    groupName="col"
+                    onDragStart={e => console.log("drag started", e)}
+                    onDragEnd={e => console.log("drag end", e)}
+                    onDrop={e => this.onCardDrop(column.id, e)}
+                    getChildPayload={index =>
+                      this.getCardPayload(column.id, index)
+                    }
+                    dragClass="card-ghost"
+                    dropClass="card-ghost-drop"
+                    onDragEnter={() => {
+                      console.log("drag enter:", column.id);
+                    }}
+                    onDragLeave={() => {
+                      console.log("drag leave:", column.id);
+                    }}
+                    onDropReady={p => console.log('Drop ready: ', p)}
+                    dropPlaceholder={{                      
+                      animationDuration: 150,
+                      showOnTop: true,
+                      className: 'drop-preview' 
+                    }}
+                    dropPlaceholderAnimationDuration={200}
+                  >
+                    {column.children.map((card:any):any => {
+                      return (
+                        <Draggable key={card.id}>
+                          <div {...card.props}>
+                            <p>{card.data}</p>
+                          </div>
+                        </Draggable>
+                      );
+                    })}
+                  </Container>
+                </div>
+              </Draggable>
+            );
+          })}
+        </Container>
+      </div>
+    );
+  }
+
+  getCardPayload(columnId:any, index:any) {
+    return this.state.scene.children.filter((p:any):any => p.id === columnId)[0].children[
+      index
+    ];
+  }
+
+  onColumnDrop(dropResult:any) {
+    const scene = Object.assign({}, this.state.scene);
+    scene.children = applyDrag(scene.children, dropResult);
+    this.setState({
+      scene
+    });
+  }
+
+  onCardDrop(columnId:any, dropResult:any) {
+    if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
+      const scene = Object.assign({}, this.state.scene);
+      const column = scene.children.filter((p:any):any => p.id === columnId)[0];
+      const columnIndex = scene.children.indexOf(column);
+
+      const newColumn = Object.assign({}, column);
+      newColumn.children = applyDrag(newColumn.children, dropResult);
+      scene.children.splice(columnIndex, 1, newColumn);
+
+      this.setState({
+        scene
+      });
+    }
+  }
+}
+
+export default Cards;
